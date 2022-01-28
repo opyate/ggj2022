e7a991cdb414a15192572cf861e27d6b00c06247
MATTHEW SHAW <ms228668@falmouth.ac.uk>
2022-01-28 20:29:36 +0000

    Added basic player movement script

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,15 +4,26 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
+
+    public float HorizontalSpeed;
+    public float JumpForce;
+
+    private Rigidbody rb;
+
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        float Horizontal = Input.GetAxis("Horizontal");
+
+        rb.velocity = new Vector3(Horizontal * -HorizontalSpeed, rb.velocity.y, 0);
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.AddForce(Vector3.up * JumpForce);
+        }
+
     }
 }
