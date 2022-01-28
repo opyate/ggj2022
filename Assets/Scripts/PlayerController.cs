15eecbcc5976c8fbdf0702823e6a374878752d2d
MATTHEW SHAW <ms228668@falmouth.ac.uk>
2022-01-28 20:39:37 +0000

    Added ground detection to stop player infini-jumping

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,6 +7,7 @@ public class PlayerController : MonoBehaviour
 
     public float HorizontalSpeed;
     public float JumpForce;
+    public float MaximumJumpSearch;
 
     private Rigidbody rb;
 
@@ -20,10 +21,17 @@ public class PlayerController : MonoBehaviour
         float Horizontal = Input.GetAxis("Horizontal");
 
         rb.velocity = new Vector3(Horizontal * -HorizontalSpeed, rb.velocity.y, 0);
+        
+        //need to do a ground check
+        //..
+        //raycast? seems like a good idea
+        RaycastHit hit;
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            rb.AddForce(Vector3.up * JumpForce);
+            if (Physics.Raycast(transform.position, Vector3.down, out hit, MaximumJumpSearch))
+            {
+                rb.AddForce(Vector3.up * JumpForce);
+            }
         }
-
     }
 }
